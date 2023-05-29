35e2859410beb9334437cfeea847d74304ffee92
Walter White <126259872+zarcorjak@users.noreply.github.com>
2023-05-28 20:48:47 -0500

    Add files via upload

---
+++ Punto 1/Boton.js
@@ -0,0 +1,4 @@
+document.getElementById("cambiarColor").addEventListener("click", function(){
+    var randomColor = Math.floor(Math.random()*16777215).toString(16);
+    document.body.style.backgroundColor = "#" + randomColor;
+  });
